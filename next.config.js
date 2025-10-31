50c0aaccb251b9f08d67334bb69a55c8d7cd62bb
robertmain53 <mainrobert53@gmail.com>
2025-10-31 05:32:15 +0100

    ca10

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
   // Compress al l responses
   compress: true,
   
-  // Optimize images
+  // Optimize  images
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
